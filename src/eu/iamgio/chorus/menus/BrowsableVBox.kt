d0f1572fde19efd72411dd2509f3049c68a53854
iAmGio <giogar2705@gmail.com>
2018-05-18 17:18:25 +0200

    Change rules of browsable menus.

--- src/eu/iamgio/chorus/menus/BrowsableVBox.kt
+++ src/eu/iamgio/chorus/menus/BrowsableVBox.kt
@@ -1,6 +1,8 @@
 package eu.iamgio.chorus.menus
 
 import eu.iamgio.chorus.Chorus
+import javafx.application.Platform
+import javafx.css.PseudoClass
 import javafx.scene.Node
 import javafx.scene.control.Button
 import javafx.scene.control.ScrollPane
@@ -10,6 +12,8 @@ import javafx.scene.input.KeyEvent
 import javafx.scene.layout.VBox
 
 
+const val HOVER_STYLE_CLASS = "bvhover"
+
 /**
  * @author Gio
  */
@@ -17,11 +21,28 @@ open class BrowsableVBox(textfield: TextField? = null) : VBox() {
 
     var scrollPane: ScrollPane? = null
 
+    private var last: Node? = null
+
     init {
         textfield?.setOnKeyPressed {
             val showing = Showables.SHOWING
             if(showing == this) {
                 browse(it, showing)
+            }
+        }
+        Platform.runLater {
+            if(Showables.SHOWING == this) {
+                children.forEach {node ->
+                    node.setOnMouseMoved {
+                        children.forEach {
+                            setHover(it, false)
+                        }
+                        setHover(node)
+                    }
+                    node.setOnMouseExited {
+                        setHover(node, false)
+                    }
+                }
             }
         }
         Chorus.getInstance().root.scene.setOnKeyReleased {
@@ -34,7 +55,7 @@ open class BrowsableVBox(textfield: TextField? = null) : VBox() {
                     val node: Node = if(children.size == 1) {
                         children[0]
                     } else {
-                        children.filtered {it.styleClass.size == 3}.firstOrNull() ?: return@setOnKeyReleased
+                        children.filtered {isHover(it)}.firstOrNull() ?: return@setOnKeyReleased
                     }
                     if(node is Button) {
                         node.fire()
@@ -48,17 +69,17 @@ open class BrowsableVBox(textfield: TextField? = null) : VBox() {
 
     private fun browse(event: KeyEvent, showing: VBox) {
         if(event.code == KeyCode.UP || event.code == KeyCode.DOWN) {
-            val selected: Node? = showing.children.filtered {it.styleClass.size == 3}.firstOrNull()
+            val selected: Node? = showing.children.filtered {isHover(it)}.firstOrNull() ?: last
             var index: Int
             if(selected == null) {
                 index = if(event.code == KeyCode.UP) lastIndex else 0
                 setHover(index)
-            }
-            else {
+            } else {
                 index = children.indexOf(selected)
                 setHover(index, false)
                 index = if(event.code == KeyCode.UP)
                     if(index == 0) lastIndex else index - 1 else if(index == lastIndex) 0 else index + 1
+                children.forEach {setHover(it, false)}
                 setHover(index)
             }
             if(scrollPane != null) {
@@ -70,15 +91,12 @@ open class BrowsableVBox(textfield: TextField? = null) : VBox() {
     private val lastIndex
         get() = children.size - 1
 
+    private fun isHover(node: Node): Boolean =
+            node.pseudoClassStates.map {it.pseudoClassName}.contains(HOVER_STYLE_CLASS)
+
     private fun setHover(node: Node, state: Boolean = true) {
-        with(node) {
-            val cls = styleClass[1] + "_keyhover"
-            if(state) {
-                if(!styleClass.contains(cls)) styleClass += cls
-            } else {
-                styleClass -= cls
-            }
-        }
+        node.pseudoClassStateChanged(PseudoClass.getPseudoClass(HOVER_STYLE_CLASS), state)
+        last = node
     }
 
     private fun setHover(i: Int, state: Boolean = true) = setHover(children[i], state)
