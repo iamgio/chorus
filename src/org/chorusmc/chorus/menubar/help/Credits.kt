d68f3238824d6d2037b1b2123a05c9a334bd1469
iAmGio <giogar2705@gmail.com>
2018-06-04 16:19:05 +0200

    Edit credits

--- src/org/chorusmc/chorus/menubar/help/Credits.kt
+++ src/org/chorusmc/chorus/menubar/help/Credits.kt
@@ -17,6 +17,7 @@ class Credits : MenuBarAction {
         val helpView = HelpView("Credits")
         helpView.addText("Created with ♥ by iAmGio, the only dev and maintainer.", true)
         helpView.addText("Thanks to beta testers: DeeJack, xQuickGlare, Pompiere1, SuperMarcomen, AlbeMiglio, SnowyCoder, AgeOfWar.")
+        helpView.addText("Thanks to Mega for the website (chorusmc.org).")
         helpView.addText("Thanks to gjkf for having helped me on RegEx.")
         helpView.addText("Thanks to everyone who donated and supported Chorus.")
         helpView.addText("Also want to thank everyone who believed in this project, especially the guys of STD.")
