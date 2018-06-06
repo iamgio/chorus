5f5d5858e8cc20883abf13de02bdc59d8bad5019
iAmGio <giogar2705@gmail.com>
2018-06-06 15:50:25 +0200

    Fix update checker

--- src/org/chorusmc/chorus/updater/Updater.kt
+++ src/org/chorusmc/chorus/updater/Updater.kt
@@ -17,21 +17,22 @@ const val URL = "https://api.github.com/repos/iAmGio/chorus/releases/latest"
 class Updater {
 
     private val parser = JSONParser(java.net.URL(URL))
-    private var parsed: JSONObject? = null
+    private var _parsed: JSONObject? = null
+    private val parsed: JSONObject
+        get() {
+            if(_parsed == null) _parsed = parser.parse()
+            return _parsed!!
+        }
 
     val isUpdatePresent: Boolean
         get() = Chorus.VERSION != latestVersion.replace("v", "")
 
     val latestVersion: String
-        get() {
-            parsed = parsed ?: parser.parse()
-            return parsed!!["tag_name"].toString()
-        }
+        get() = parsed["tag_name"].toString()
 
     fun downloadLatest(type: Int): Pair<Status, File?> {
         val folder = ChorusFolder.RELATIVE
-        parsed = parsed ?: parser.parse()
-        val fileStr = ((parsed!!["assets"] as JSONArray)[type] as JSONObject)["browser_download_url"].toString()
+        val fileStr = ((parsed["assets"] as JSONArray)[type] as JSONObject)["browser_download_url"].toString()
         val file = File(folder, fileStr.split("/").last())
         if(file.exists()) return Status.ALREADY_EXISTS to file
         return try {
