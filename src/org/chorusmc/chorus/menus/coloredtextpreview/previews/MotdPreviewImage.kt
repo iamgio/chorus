f3c34e57aa6f7c2c1717d119549de1767ecba158
iAmGio <giogar2705@gmail.com>
2018-06-14 15:31:55 +0200

    Make MOTD preview more realistic

--- src/org/chorusmc/chorus/menus/coloredtextpreview/previews/MotdPreviewImage.kt
+++ src/org/chorusmc/chorus/menus/coloredtextpreview/previews/MotdPreviewImage.kt
@@ -1,11 +1,11 @@
 package org.chorusmc.chorus.menus.coloredtextpreview.previews
 
+import javafx.scene.image.Image
+import javafx.scene.text.TextFlow
 import org.chorusmc.chorus.Chorus
 import org.chorusmc.chorus.minecraft.chat.ChatParser
 import org.chorusmc.chorus.util.toFlowList
 import org.chorusmc.chorus.util.withStyleClass
-import javafx.scene.image.Image
-import javafx.scene.text.TextFlow
 
 /**
  * @author Gio
@@ -13,8 +13,8 @@ import javafx.scene.text.TextFlow
 class MotdPreviewImage(title: String, first: String) : ColoredTextPreviewImage(
         ColoredTextBackground(Image(Chorus::class.java.getResourceAsStream("/assets/minecraft/previews/motd-background.png"))),
         listOf(
-                ChatParser(title, true).toTextFlow().withStyleClass("minecraft-motd-preview-flow"),
-                ChatParser(first, true).toTextFlow().withStyleClass("minecraft-motd-preview-flow"),
+                ChatParser(title, true).toTextFlow(false).withStyleClass("minecraft-motd-preview-flow"),
+                ChatParser(first, true).toTextFlow(false).withStyleClass("minecraft-motd-preview-flow"),
                 TextFlow().withStyleClass("minecraft-motd-preview-flow")
         ).toFlowList()
 ) {
@@ -22,6 +22,6 @@ class MotdPreviewImage(title: String, first: String) : ColoredTextPreviewImage(
     override fun initFlow(flow: TextFlow, index: Int) {
         styleClass += "minecraft-motd-preview-flow"
         flow.layoutX = 65.0
-        flow.layoutY = 5.0 + (index * (50 / 3))
+        flow.layoutY = index * (50.0 / 3.0)
     }
 }
--- src/org/chorusmc/chorus/menus/drop/actions/previews/MOTDPreview.kt
+++ src/org/chorusmc/chorus/menus/drop/actions/previews/MOTDPreview.kt
@@ -1,11 +1,11 @@
 package org.chorusmc.chorus.menus.drop.actions.previews
 
+import javafx.scene.control.TextField
 import org.chorusmc.chorus.editor.EditorArea
 import org.chorusmc.chorus.menus.coloredtextpreview.ColoredTextPreviewMenu
 import org.chorusmc.chorus.menus.coloredtextpreview.previews.MotdPreviewImage
 import org.chorusmc.chorus.menus.drop.actions.DropMenuAction
 import org.chorusmc.chorus.minecraft.chat.ChatParser
-import javafx.scene.control.TextField
 
 /**
  * @author Gio
@@ -21,13 +21,13 @@ class MOTDPreview : DropMenuAction() {
         second.promptText = "Second"
         val menu = ColoredTextPreviewMenu("MOTD preview", MotdPreviewImage(title.text, first.text), listOf(title, first, second))
         title.textProperty().addListener {_ ->
-            menu.image.flows[0] = ChatParser(title.text, true).toTextFlow()
+            menu.image.flows[0] = ChatParser(title.text, true).toTextFlow(false)
         }
         first.textProperty().addListener {_ ->
-            menu.image.flows[1] = ChatParser(first.text, true).toTextFlow()
+            menu.image.flows[1] = ChatParser(first.text, true).toTextFlow(false)
         }
         second.textProperty().addListener {_ ->
-            menu.image.flows[2] = ChatParser(second.text, true).toTextFlow()
+            menu.image.flows[2] = ChatParser(second.text, true).toTextFlow(false)
         }
         menu.toFocus = 1
         menu.layoutX = x
