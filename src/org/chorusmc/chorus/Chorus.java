c207b62b8a8389cae1f26868ec48c19d5164c880
iAmGio <giogar2705@gmail.com>
2018-06-17 21:55:52 +0200

    Change version to 1.1.0 (not publishing yet, I want to troll you :) )

--- src/org/chorusmc/chorus/Chorus.java
+++ src/org/chorusmc/chorus/Chorus.java
@@ -33,7 +33,7 @@ import java.util.Arrays;
  */
 public class Chorus extends FXApplication {
 
-    public static final String VERSION = "1.0.3";
+    public static final String VERSION = "1.1.0";
 
     private static File passedFile;
 
