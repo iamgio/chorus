77b60602f486dbd1a71c3abc6e5fa2e8e778d3a4
iAmGio <giogar2705@gmail.com>
2018-07-17 18:05:41 +0200

    Remove 'Insert -> [...] ID' if a non-ID-based version is selected

--- src/org/chorusmc/chorus/menus/drop/InsertDropMenu.kt
+++ src/org/chorusmc/chorus/menus/drop/InsertDropMenu.kt
@@ -1,21 +1,28 @@
 package org.chorusmc.chorus.menus.drop
 
+import org.chorusmc.chorus.minecraft.McClass
+
 /**
  * @author Gio
  */
 class InsertDropMenu : DropMenu() {
 
-    override fun getButtons(): MutableList<DropMenuButton> = arrayListOf(
-            DropMenuButton("Colored text", "insert"),
-            DropMenuButton("Item name", "insert"),
-            DropMenuButton("Item ID", "insert"),
-            DropMenuButton("Particle name", "insert"),
-            DropMenuButton("Effect name", "insert"),
-            DropMenuButton("Effect ID", "insert"),
-            DropMenuButton("Sound name", "insert"),
-            DropMenuButton("Entity name", "insert"),
-            DropMenuButton("Enchantment name", "insert"),
-            DropMenuButton("Enchantment ID", "insert"),
-            DropMenuButton("Ticks", "insert")
-    )
+    override fun getButtons(): MutableList<DropMenuButton> {
+        val array = arrayListOf(
+                DropMenuButton("Colored text", "insert"),
+                DropMenuButton("Item name", "insert"),
+                DropMenuButton("Particle name", "insert"),
+                DropMenuButton("Effect name", "insert"),
+                DropMenuButton("Sound name", "insert"),
+                DropMenuButton("Entity name", "insert"),
+                DropMenuButton("Enchantment name", "insert"),
+                DropMenuButton("Ticks", "insert")
+        )
+        if(McClass("").version == "1.12") {
+            array.add(2, DropMenuButton("Item ID", "insert"))
+            array.add(5, DropMenuButton("Effect ID", "insert"))
+            array.add(9, DropMenuButton("Enchantment ID", "insert"))
+        }
+        return array
+    }
 }
