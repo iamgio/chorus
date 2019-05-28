53ab9487f7505ea0267885c560d9c2c2d56b49d8
iAmGio <giogar2705@gmail.com>
2019-05-28 18:03:02 +0200

    Make Help windows resizable

--- src/org/chorusmc/chorus/views/HelpView.kt
+++ src/org/chorusmc/chorus/views/HelpView.kt
@@ -1,13 +1,14 @@
 package org.chorusmc.chorus.views
 
 import eu.iamgio.libfx.util.Roots
-import javafx.fxml.FXMLLoader
 import javafx.scene.Node
 import javafx.scene.Scene
 import javafx.scene.control.Label
 import javafx.scene.image.Image
 import javafx.scene.layout.AnchorPane
+import javafx.scene.layout.Region
 import javafx.scene.layout.VBox
+import javafx.scene.shape.Rectangle
 import javafx.stage.Stage
 import org.chorusmc.chorus.Chorus
 import org.chorusmc.chorus.theme.Themes
@@ -21,18 +22,38 @@ class HelpView(private val title: String) {
 
     fun show() {
         val stage = Stage()
-        val root = FXMLLoader.load<AnchorPane>(Chorus::class.java.getResource("/assets/views/Help.fxml"))
+        val root = AnchorPane()
+        root.styleClass.addAll("pane", "help-pane")
+        val rectangle = Rectangle()
+        rectangle.styleClass += "help-rectangle"
+        rectangle.widthProperty().bind(root.widthProperty())
+        rectangle.height = 175.0
+        val titleLabel = Label()
+        titleLabel.id = "title"
+        titleLabel.layoutX = 15.0
+        titleLabel.layoutY = 40.0
+        titleLabel.styleClass += "help-title"
+        val vbox = VBox(20.0)
+        vbox.id = "vbox"
+        vbox.style = "-fx-padding: 0 25 0 25"
+        vbox.layoutY = 200.0
+        root.children.addAll(rectangle, titleLabel, vbox)
         val scene = Scene(root, 670.0, 650.0)
         scene.stylesheets.addAll(Themes.byConfig().path[0], "/assets/styles/global.css")
         stage.minWidth = scene.width
         stage.minHeight = scene.height
-        stage.isResizable = false
         stage.title = "Chorus - $title"
         stage.scene = scene
         stage.icons += Image(Chorus::class.java.getResourceAsStream("/assets/images/icon.png"))
         stage.show()
         (Roots.getById(root, "title") as Label).text = title
-        (Roots.getById(root, "vbox") as VBox).children.addAll(nodes)
+        (Roots.getById(root, "vbox") as VBox).children.addAll(nodes.map {
+            if(it is Region) {
+                it.prefWidthProperty().bind(root.widthProperty().subtract(40))
+                it.maxWidthProperty().bind(it.prefWidthProperty())
+            }
+            it
+        })
     }
 
     fun addNode(node: Node) {
