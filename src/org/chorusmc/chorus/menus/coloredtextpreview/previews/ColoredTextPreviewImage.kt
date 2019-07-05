a31fae11a9312eaaba682c7bc9f2ccdd499bff9a
iAmGio <giogar2705@gmail.com>
2019-07-05 16:17:02 +0200

    Update ColoredTextPreviewImage height

--- src/org/chorusmc/chorus/menus/coloredtextpreview/previews/ColoredTextPreviewImage.kt
+++ src/org/chorusmc/chorus/menus/coloredtextpreview/previews/ColoredTextPreviewImage.kt
@@ -36,6 +36,7 @@ abstract class ColoredTextPreviewImage @JvmOverloads constructor(
         @Suppress("LEAKINGTHIS")
         flows.image = this
         prefWidth = background.width
+        prefHeight = background.height
         background.onWidthChange = Runnable {
             prefWidth = background.width
         }
