ad3784b795e6be71edc8cdcaad4c236abbe94e48
iAmGio <giogar2705@gmail.com>
2019-07-12 17:21:31 +0200

    JS API: implement connect function

--- src/assets/js/lib.js
+++ src/assets/js/lib.js
@@ -420,7 +420,30 @@ function setTheme(name, internal) {
  * @return java.io.File
  */
 function toFile(file) {
-    return typeof file == 'string' ? new File(file, getFolder()) : file
+    return typeof file == 'string' ? new File(file, getFolder()) : file;
+}
+
+// --- HTTP --- //
+
+/**
+ * Override this to set a user-agent for connections
+ */
+var userAgent;
+
+/**
+ * Establishes an HTTP connection
+ * @param url target url
+ * @param data (optional) parameters as map
+ * @return org.jsoup.Connection
+ */
+function connect(url, data) {
+    var connection = org.jsoup.Jsoup.connect(url);
+    if(data) {
+        for(param in data) {
+            connection = connection.data(param, data[param]);
+        }
+    }
+    return userAgent ? connection.userAgent(userAgent) : connection;
 }
 
 // --- TYPE UTILITIES --- //
