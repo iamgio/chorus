a822a383a41ff160ccad9010d259ffce57f0368b
iAmGio <giogar2705@gmail.com>
2020-04-05 15:23:39 +0200

    Optimize notifications

--- src/org/chorusmc/chorus/notification/Notification.kt
+++ src/org/chorusmc/chorus/notification/Notification.kt
@@ -1,13 +1,15 @@
 package org.chorusmc.chorus.notification
 
-import org.chorusmc.chorus.editor.EditorController
 import eu.iamgio.libfx.animations.Animation
 import eu.iamgio.libfx.timing.WaitingTimer
-import eu.iamgio.libfx.util.Roots
 import javafx.application.Platform
+import javafx.geometry.Insets
 import javafx.scene.control.Label
-import javafx.scene.shape.Rectangle
+import javafx.scene.layout.Background
+import javafx.scene.layout.BackgroundFill
+import javafx.scene.layout.CornerRadii
 import javafx.util.Duration
+import org.chorusmc.chorus.Chorus
 
 /**
  * @author Gio
@@ -16,24 +18,23 @@ class Notification(private val text: String, private val type: NotificationType)
 
     fun send() {
         Platform.runLater {
-            val root = EditorController.getInstance().root
-            val rectangle = Roots.getById(root, "notification-rectangle") as Rectangle
-            val label = Roots.getById(root, "notification-label") as Label
-            label.text = text
-            rectangle.widthProperty().bind(root.widthProperty())
-            rectangle.fill = type.paint
+            val root = Chorus.getInstance().root
+            val label = Label(text).apply {
+                translateY = -70.0
+                prefWidthProperty().bind(root.prefWidthProperty())
+                padding = Insets(20.0)
+                style = "-fx-text-fill: white; -fx-font-size: 18; -fx-font-weight: bold; -fx-effect: dropshadow(gaussian, rgba(0, 0, 0, .4), 10, 0, 0, 5)"
+                background = Background(BackgroundFill(type.paint, CornerRadii.EMPTY, Insets.EMPTY))
+            }
+            root.children += label
 
-            var rectangleAnimation = Animation(Animation.Type.MOVEMENT_Y, 0.0, Duration.seconds(.25))
-            var labelAnimation = Animation(Animation.Type.MOVEMENT_Y, 17.5, Duration.seconds(.25))
-            rectangleAnimation.play(rectangle)
-            labelAnimation.play(label)
-
-            rectangleAnimation.setOnAnimationCompleted {
+            var animation = Animation(Animation.Type.MOVEMENT_Y, 0.0, Duration.seconds(.25))
+            animation.play(label)
+            animation.setOnAnimationCompleted {
                 WaitingTimer().start({
-                    rectangleAnimation = Animation(Animation.Type.MOVEMENT_Y, -75.0, Duration.seconds(.1))
-                    labelAnimation = Animation(Animation.Type.MOVEMENT_Y, -57.5, Duration.seconds(.1))
-                    rectangleAnimation.play(rectangle)
-                    labelAnimation.play(label)
+                    animation = Animation(Animation.Type.MOVEMENT_Y, -75.0, Duration.seconds(.1))
+                    animation.play(label)
+                    animation.setOnAnimationCompleted {root.children -= label}
                 }, type.duration)
             }
         }
