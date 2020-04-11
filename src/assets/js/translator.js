4a2cc06dfb45357fd7c41254ac4d6dee69648628
iAmGio <giogar2705@gmail.com>
2020-04-11 12:32:40 +0200

    Update translator.js

--- src/assets/js/translator.js
+++ src/assets/js/translator.js
@@ -6,13 +6,14 @@
  * @return java.lang.String
  */
 function translateFromWeb(from, to, text) {
-    var doc = connect('https://engine2-async.reverso.net/WebReferences/WSAJAXInterface.asmx/TranslateCorrWS')
-        .requestBody("{'searchText': '" + text.replaceAll("\\bi\\b", "I").replace("'", "\\'") + "', 'direction': '" + from + "-" + to + "-2', 'maxTranslationChars':'-1', 'usecorr':'true'}")
+    var doc = connect('https://async.reverso.net/WebReferences/WSAJAXInterface.asmx/TranslateCorrWS')
+        .requestBody("{'searchText': '" + text.replaceAll("\\bi\\b", "I").replace("'", "\\'") + "', 'direction': '" + from + "-" + to + "-7', 'maxTranslationChars':'-1', 'usecorr':'true'}")
         .ignoreHttpErrors(true)
         .ignoreContentType(true)
         .userAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0')
         .header('Content-Type', 'application/json')
         .referrer('http://www.reverso.net/')
         .post();
+    print(doc.body())
     return json(doc.body().text()).get("d").get("result");
 }
