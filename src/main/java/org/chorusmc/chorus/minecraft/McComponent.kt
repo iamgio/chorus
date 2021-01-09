919e20c44b4da7102cd5eaf2d5e263e22f20ddaa
Giorgio Garofalo <giogar2705@gmail.com>
2021-01-09 16:46:57 +0100

    Add McComponent#formalName

--- src/main/java/org/chorusmc/chorus/minecraft/McComponent.kt
+++ src/main/java/org/chorusmc/chorus/minecraft/McComponent.kt
@@ -1,9 +1,14 @@
 package org.chorusmc.chorus.minecraft
 
+import org.chorusmc.chorus.util.makeFormal
+
 /**
  * @author Giorgio Garofalo
  */
 interface McComponent {
 
     val name: String
+
+    val formalName: String
+        get() = name.makeFormal()
 }
