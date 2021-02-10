fab5cf9512fd837241e8d51ed9ba20d1a49bac19
Giorgio Garofalo <giogar2705@gmail.com>
2021-02-10 16:40:05 +0100

    JS API: Implement onTabClose

--- src/main/java/org/chorusmc/chorus/nodes/Tab.kt
+++ src/main/java/org/chorusmc/chorus/nodes/Tab.kt
@@ -3,6 +3,7 @@ package org.chorusmc.chorus.nodes
 import javafx.beans.property.SimpleObjectProperty
 import javafx.scene.Node
 import javafx.scene.control.Tab
+import org.chorusmc.chorus.addon.Addons
 import org.chorusmc.chorus.editor.EditorArea
 import org.chorusmc.chorus.editor.EditorController
 import org.chorusmc.chorus.file.ChorusFile
@@ -18,6 +19,7 @@ class Tab(text: String, content: Node, val file: ChorusFile) : Tab("$text ", con
         setOnCloseRequest {
             tabs.remove(file.absolutePath)
             close(false)
+            Addons.invoke("onTabClose", this)
         }
     }
 
