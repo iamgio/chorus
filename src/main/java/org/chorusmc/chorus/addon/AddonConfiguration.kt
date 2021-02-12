98edda2b67df0cbacafa65723e9f94a9e53fe0b7
Giorgio Garofalo <giogar2705@gmail.com>
2021-02-12 10:18:55 +0100

    Implement nested access to add-on configurations

--- src/main/java/org/chorusmc/chorus/addon/AddonConfiguration.kt
+++ src/main/java/org/chorusmc/chorus/addon/AddonConfiguration.kt
@@ -34,14 +34,26 @@ class AddonConfiguration : ChorusConfiguration("config.yml") {
         map = config.load(FileInputStream(target)) ?: hashMapOf()
     }
 
+    // Example: a.b.c.d -> {a{b{c}}} paired d
+    private fun getNestedMap(key: String, map: Map<String, Any> = this.map): Pair<Map<String, Any>, String> {
+        if(key.indexOf(".", 1) == -1) {
+            return map to key.substring(key.lastIndexOf(".") + 1)
+        }
+        val branch = key.substring(0, key.indexOf("."))
+        val next = key.substring(branch.length + 1)
+
+        @Suppress("UNCHECKED_CAST")
+        return getNestedMap(next, map[branch] as Map<String, Any>)
+    }
+
     override fun getKeys() = map.keys
 
-    override fun get(key: String) = map[key]
+    override fun get(key: String) = getNestedMap(key).let { it.first[it.second] }
 
-    fun getList(key: String) = map[key] as List<Any?>
+    fun getList(key: String) = get(key) as List<Any?>
 
     @Suppress("UNCHECKED_CAST")
-    fun getMap(key: String) = map[key] as Map<String, Any?>
+    fun getMap(key: String) = get(key) as Map<String, Any?>
 
     override fun set(key: String, value: Any) {
         setWithoutSaving(key, value)
@@ -49,7 +61,9 @@ class AddonConfiguration : ChorusConfiguration("config.yml") {
     }
 
     override fun setWithoutSaving(key: String, value: Any) {
-        map[key] = value
+        getNestedMap(key).let {
+            (it.first as HashMap)[it.second] = value
+        }
     }
 
     override fun store() {
