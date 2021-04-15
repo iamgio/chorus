8531c8f899081f232d924d98e8dbee571d321534
Giorgio Garofalo <giogar2705@gmail.com>
2021-04-15 12:26:24 +0200

    Fix Chinese not loading by setting lowercase language tag

--- src/main/java/org/chorusmc/chorus/lang/Lang.kt
+++ src/main/java/org/chorusmc/chorus/lang/Lang.kt
@@ -9,7 +9,7 @@ enum class Lang(val tag: String, private val commonName: String) {
     ENGLISH("en", "English"),
     ITALIAN("it", "Italiano"),
     GERMAN("de", "Deutsch"),
-    SCHINESE("zhCN", "简体中文");
+    SCHINESE("zhcn", "简体中文");
 
     companion object {
         @JvmStatic fun fromCommonName(name: String) = values().first {it.commonName.equals(name, true)}
