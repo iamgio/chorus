b9cdc1600de06a32864bc5315a9f9e8973841e51
Giorgio Garofalo <giogar2705@gmail.com>
2021-04-20 17:54:59 +0200

    Fix armor color picker color

--- src/main/java/org/chorusmc/chorus/menus/drop/actions/previews/ArmorPreview.kt
+++ src/main/java/org/chorusmc/chorus/menus/drop/actions/previews/ArmorPreview.kt
@@ -25,6 +25,7 @@ class ArmorPreview : DropMenuAction() {
         textfield.text = toDecimal(picker.value.toString()).toString()
         picker.valueProperty().addListener {_ -> textfield.text = toDecimal(picker.value.toString()).toString()}
         picker.prefWidth = textfield.prefWidth
+        picker.styleClass += "text-field"
         val menu = ColoredTextPreviewMenu(translate("preview.armor"), ArmorPreviewImage(picker.value), listOf(textfield, picker))
         picker.valueProperty().addListener {_ ->
             menu.image.background.paint = picker.value
