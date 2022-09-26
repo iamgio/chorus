17d23e91bc88cdea479248e201ecac2a2e7c2e72
Giorgio Garofalo <giogar2705@gmail.com>
2022-09-26 19:10:00 +0200

    Improve auto-saving

--- src/main/java/org/chorusmc/chorus/listeners/AutoSavingListener.kt
+++ src/main/java/org/chorusmc/chorus/listeners/AutoSavingListener.kt
@@ -1,33 +1,62 @@
 package org.chorusmc.chorus.listeners
 
 import eu.iamgio.libfx.timing.RepeatingTimer
+import javafx.application.Platform
 import javafx.util.Duration
 import org.chorusmc.chorus.editor.EditorArea
+import org.chorusmc.chorus.settings.ObservableBooleanSettingsProperty
+import org.chorusmc.chorus.settings.ObservableIntSettingsProperty
+import org.chorusmc.chorus.settings.SettingsBuilder
 import org.chorusmc.chorus.util.config
 import org.fxmisc.richtext.model.PlainTextChange
+import java.util.Timer
+import java.util.TimerTask
+import kotlin.concurrent.timerTask
 
 /**
  * @author Giorgio Garofalo
  */
 class AutoSavingListener : EditorEvent() {
 
-    private val timer = RepeatingTimer()
-    private val areas = ArrayList<EditorArea>()
-    private val editedAreas = ArrayList<EditorArea>()
+    private val enabledYaml by ObservableBooleanSettingsProperty("2.Autosaving.1.Enabled_(YAML)")
+    private val enabledOthers by ObservableBooleanSettingsProperty("2.Autosaving.2.Enabled_(other_formats)")
+    private var delay: Int = 3500
+
+    private var activeTimerTask: TimerTask? = null
+    private val editedAreas = mutableListOf<EditorArea>()
+
+    init {
+        // Restart the timer when the delay is changed
+        "2.Autosaving.3.Delay_(ms)".let {
+            delay = config.getInt(it)
+
+            SettingsBuilder.addAction(it, {
+                delay = config.getInt(it)
+                activeTimerTask?.cancel()
+                startTimer()
+            })
+        }
+    }
+
+    private fun startTimer() {
+        activeTimerTask = timerTask {
+            for(area in editedAreas) {
+                if(!area.file.closed) {
+                    Platform.runLater {
+                        area.saveFile()
+                        editedAreas -= area
+                    }
+                }
+            }
+        }
+        Timer().scheduleAtFixedRate(activeTimerTask, 0L, delay.toLong())
+    }
 
     override fun onChange(change: PlainTextChange, area: EditorArea) {
-        if((area.supportsHighlighting() && config.getBoolean("2.Autosaving.1.Enabled_(YAML)")) ||
-                !area.supportsHighlighting() && config.getBoolean("2.Autosaving.2.Enabled_(other_formats)")) {
-            editedAreas.add(area)
-            if(!areas.contains(area)) {
-                timer.start({
-                    if(editedAreas.contains(area) && !area.file.closed) {
-                        area.saveFile()
-                        editedAreas.remove(area)
-                    }
-                }, Duration.millis(config.getInt("2.Autosaving.3.Delay_(ms)").toDouble()))
-                areas.add(area)
-            }
+        if(area in editedAreas) return
+        if(activeTimerTask == null) startTimer()
+        if((area.supportsHighlighting() && enabledYaml) || !area.supportsHighlighting() && enabledOthers) {
+            editedAreas += area
         }
     }
 }
